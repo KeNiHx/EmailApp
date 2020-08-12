8ab04f08c0ab3a64530b2a2618c5df1fb8945beb
KeNiHx <klam291@mtroyal.ca>
2020-08-11 20:10:46 -0600

    starting files

---
+++ src/com/company/klam/Email.java
@@ -0,0 +1,34 @@
+package com.company.klam;
+
+public class Email {
+    //Want to encapsulate
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive first name and last name
+    public Email (String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+
+
+        //Call a method asking for the department = return the department
+    }
+
+    //Ask for the department
+
+    //Generate random password
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+    //Change the password
+
+
+
+}
---
+++ src/com/company/klam/Main.java
@@ -0,0 +1,10 @@
+package com.company.klam;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //create an object
+        Email em1 = new Email("John","Smith");
+
+    }
+}
