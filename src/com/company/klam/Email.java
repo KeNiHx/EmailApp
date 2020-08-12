2386258ecd24720337740d04ef176b6df7ae1603
KeNiHx <klam291@mtroyal.ca>
2020-08-11 20:53:20 -0600

    adding method to set email department

--- src/com/company/klam/Email.java
+++ src/com/company/klam/Email.java
@@ -1,5 +1,7 @@
 package com.company.klam;
 
+import java.util.Scanner;
+
 public class Email {
     //Want to encapsulate
     private String firstName;
@@ -17,9 +19,27 @@ public class Email {
 
 
         //Call a method asking for the department = return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
 
     //Ask for the department
+    private String setDepartment() {
+        System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Developement\n3 for Accounting\n0 for none\n Enter department code: ");
+        Scanner in = new Scanner(System.in);
+        int deptChoice = in.nextInt();
+        if(deptChoice == 1){
+            return "sales";
+        }
+        else if(deptChoice == 2){
+            return "dev";
+        }
+        else if(deptChoice == 3){
+            return "acct";
+        }else{
+            return "";
+        }
+    }
 
     //Generate random password
 
